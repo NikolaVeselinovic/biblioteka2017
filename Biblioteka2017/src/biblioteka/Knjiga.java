a433bb3363f8f6bdf12d72ad17a0d33e46e80adf
NikolaVeselinovic <nikolaveselinovic388@gmail.com>
2017-03-14 15:35:30 +0100

    Doodate toString, hashCode i equals u klase knjige i autor

--- Biblioteka2017/src/biblioteka/Knjiga.java
+++ Biblioteka2017/src/biblioteka/Knjiga.java
@@ -25,6 +25,31 @@ public class Knjiga {
 	public String getIzdavac() {
 		return izdavac;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (isbn ^ (isbn >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		if (isbn != other.isbn)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", izdavac=" + izdavac + ", izdanje=" + izdanje
+				+ ", autori=" + autori + "]";
+	}
 	public void setIzdavac(String izdavac) {
 		this.izdavac = izdavac;
 	}
